8ded133c1d1381f69d3690a061450236aa141ca4
Matei Copot <matei@copot.eu>
2017-03-09 16:17:07 +0000

    added multiple callbacks

--- index.js
+++ index.js
@@ -12,16 +12,24 @@ $.url = function( str ){
   this.url = str;
 }
 
-$.methods = [ 'all', 'get', 'post' ];
+$.methods = [ 'all', 'get', 'post', 'put', 'delete' ];
 $.methods.map( function( method ){
-  $.url.prototype[ method ] = function( callback ){
-    app[ method ]( this.url, $.callback( this, callback ) );
+  $.url.prototype[ method ] = function( argument ){
+
+    if( argument.constructor === Function )
+      app[ method ]( this.url, $.callback( this, argument ) );
+
+    if( argument.constructor === Array )
+      argument.map( ( handler ) =>
+        app[ method ]( this.url, $.callback( this, handler ) )
+      );
   }
 });
 $.callback = function( owner, fn ){
-  return (function( req, res ){
+  return (function( req, res, next ){
     owner.req = req;
     owner.res = res;
+    owner.next = next;
 
     fn.bind( owner )();
   } ).bind( owner )
